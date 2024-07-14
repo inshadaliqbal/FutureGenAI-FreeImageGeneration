b6b6da45998d709ba12e055af26456de0093b778
Inshad <inshadaliqbal786@gmail.com>
2024-07-14 21:46:21 +0530

    all major functions done

--- lib/pages/registration_page.dart
+++ lib/pages/registration_page.dart
@@ -41,7 +41,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                     Padding(
                       padding: const EdgeInsets.only(bottom: 150.0, top: 50),
                       child: Text(
-                        'Welcome!',
+                        'Welcome! ',
                         style: kMainTextstyleWelcomePage,
                       ),
                     ),
@@ -56,7 +56,7 @@ class _RegistrationPageState extends State<RegistrationPage> {
                       },
                     ),
                     MainTextFieldPassword(
-                      label: 'PASSWORD',
+                      label: 'PASSWORD ',
                       changeFunction: (value) {
                         password = value;
                       },
